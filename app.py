3d2f22d47c8fcde5aca0dcdb9626de75090c1ad4
subjadeites <sublunary95@gmail.com>
2022-08-20 23:44:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from app import create_app
 
+app = create_app()
+
+# region 以下是抄的，不知道是啥，能用就行
+# set default button sytle and size, will be overwritten by macro parameters
+app.config['BOOTSTRAP_BTN_STYLE'] = 'primary'
+app.config['BOOTSTRAP_BTN_SIZE'] = 'sm'
+# app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'lumen'  # uncomment this line to test bootswatch theme
+
+# set default icon title of table actions
+app.config['BOOTSTRAP_TABLE_VIEW_TITLE'] = 'Read'
+app.config['BOOTSTRAP_TABLE_EDIT_TITLE'] = 'Update'
+app.config['BOOTSTRAP_TABLE_DELETE_TITLE'] = 'Remove'
+app.config['BOOTSTRAP_TABLE_NEW_TITLE'] = 'Create'
+# endregion
+
 if __name__ == '__main__':
-    app = create_app()
-
-    # region 以下是抄的，不知道是啥，能用就行
-    # set default button sytle and size, will be overwritten by macro parameters
-    app.config['BOOTSTRAP_BTN_STYLE'] = 'primary'
-    app.config['BOOTSTRAP_BTN_SIZE'] = 'sm'
-    # app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'lumen'  # uncomment this line to test bootswatch theme
-
-    # set default icon title of table actions
-    app.config['BOOTSTRAP_TABLE_VIEW_TITLE'] = 'Read'
-    app.config['BOOTSTRAP_TABLE_EDIT_TITLE'] = 'Update'
-    app.config['BOOTSTRAP_TABLE_DELETE_TITLE'] = 'Remove'
-    app.config['BOOTSTRAP_TABLE_NEW_TITLE'] = 'Create'
-    # endregion
-
     app.run(debug=False, host='127.0.0.1', port=6000)
