a0fe1032c3ab562d1aba2d172ca2b67d4abffb96
subjadeites <sublunary95@gmail.com>
2022-08-20 23:24:18 +0800

    gunicorn and create logs folder

---
+++ gun.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# cython:language_level=3
+# @Time    : 2022/8/20 23:22
+# @Author  : subjadeites
+# @File    : gun.py
+import gevent.monkey
+gevent.monkey.patch_all()
+
+import multiprocessing
+
+debug = True
+loglevel = 'deubg'
+bind = '0.0.0.0:5000'
+pidfile = 'logs/gunicorn.pid'
+logfile = 'logs/debug.log'
+
+#启动的进程数
+workers = multiprocessing.cpu_count()
+worker_class = 'gunicorn.workers.ggevent.GeventWorker'
+
+x_forwarded_for_header = 'X-FORWARDED-FOR'
---
+++ logs/__init__.py
@@ -0,0 +1,6 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# cython:language_level=3
+# @Time    : 2022/8/20 23:23
+# @Author  : subjadeites
+# @File    : __init__.py.py
